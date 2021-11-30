4cc35fdb5da97d280ca2e9f0b3f98e3199a3b45c
AndreUdotai <ubongudotai@gmail.com>
2021-11-30 23:53:17 +0100

    created js and css files for preview component

--- src/components/preview.js
+++ src/components/preview.js
@@ -3,7 +3,11 @@ import '../styles/preview.css';
 
 class Preview extends React.Component {
     render() {
-        return <div>PREVIEW</div>;
+        return (
+            <div className='row my-4 w75 mx-auto'>
+                PREVIEWS
+            </div>
+        )
     }
 }
 
