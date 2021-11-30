4740e8488c9901b51eef3be09b063b8454a49efb
AndreUdotai <ubongudotai@gmail.com>
2021-11-30 17:02:44 +0100

    started and added the body component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/header';
-// import Body from './components/body';
+import Body from './components/body';
 
 class App extends React.Component {
     render() {
         return (
             <div>
                 <Header />
+                <Body />
             </div>
         );
     }
---
+++ src/components/body.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// import Mode from './components/mode';
+// import Editor from './components/editor';
+// import Preview from './components/preview';
+import '../styles/body.css'
+
+class Body extends React.Component{
+    render(){
+        return(
+            <div>
+                {/*< Mode />
+                < Editor />
+                < Preview />*/}
+            </div>
+        )
+    }
+}
+
+export default Body;
